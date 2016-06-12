a0cecff573ffc36add9c166f4cb3bb6d895440f5
Dave Cross <dave@dave.org.uk>
2016-06-12 08:46:24 +0100

    Bumped version for release

--- lib/Array/Compare.pm
+++ lib/Array/Compare.pm
@@ -187,7 +187,7 @@ use Moo;
 use Types::Standard qw(Str Bool HashRef);
 use Carp;
 
-$VERSION = '2.11';
+$VERSION = '2.12';
 
 has Sep        => ( is => 'rw', isa => Str,     default => '^G' );
 has WhiteSpace => ( is => 'rw', isa => Bool,    default => 1 );
